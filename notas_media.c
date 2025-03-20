4291a39d216d23659d446c1ceaec2b0107d73485
jvcosta-hub <jvcostamg@gmail.com>
2025-03-20 18:46:22 +0000

    inicial

---
+++ notas_media.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+
+    int nota1, nota2, nota3;
+    int media;
+
+    // Entrada e saida de dados
+
+    printf("Insira a primeira nota: ");
+    scanf("%d", &nota1);
+
+    printf("Insira a segunda nota: ");
+    scanf("%d", &nota2);
+
+    printf("Insira a terceira nota: ");
+    scanf("%d", &nota3);
+
+    // Calculando a média
+
+    media = (float) (nota1 + nota2 + nota3) / 3;
+    
+    printf("A média das notas é : %f\n", media);
+
+    return 0;
+}
