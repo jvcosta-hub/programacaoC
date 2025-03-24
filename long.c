bdae1cef33264fe0a889f7c1aeba8503215a9ab4
jvcosta-hub <jvcostamg@gmail.com>
2025-03-24 18:54:51 +0000

    exercicios com long

---
+++ long.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+        int numeroNormal = 2147483647; // Valor máximo de int
+        long int numeroGrande = 2147483647;
+
+        printf("Numero regular (int): %d\n", numeroNormal);
+        printf("Numero grande (long int): %ld\n");
+
+        return 0;
+
+
+
+
+}
