f4f432ef5718f066e01cd8b34f3f4f52cd1fd0d1
jvcosta-hub <jvcostamg@gmail.com>
2025-04-02 20:26:27 +0000

    identificador Par ou impar

--- Condicionais/cond_par.c
+++ Condicionais/cond_par.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(){
+    int numero;
+        printf("Digite um número: ");
+            scanf("%d", &numero);
+
+                if(numero % 2 == 0){
+                    printf("\nO número é Par!\n");
+                }
+                if(numero % 2 == 1){
+                    printf("\nO número é Impar!\n");
+                }
+                    return 0;
+
+
+}
