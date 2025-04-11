ed0424ce3d6b782a00c76dbf975c9e892c9f624b
jvcosta-hub <jvcostamg@gmail.com>
2025-04-11 18:51:35 +0000

    seguindo

--- Condicionais/idade_relese.c
+++ Condicionais/idade_relese.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+int main(){
+//Criança < 12
+//Adolescente 12 <= x , 18
+//Adulto 18 <=
 
+int idade;
+
+printf("Digite sua idade: ");
+scanf("%d", &idade);
+
+    if(idade < 12){
+    prinf("Pra mim vc é CRIANÇA !");
+    } else if(idade >= 12 && idade < 18 ){
+    printf("Pra mim vc é ADOLESCENTE!");
+    } else { 
+     printf("Para mim vc é VELHINHO!");
+    } 
+
+return 0;
+
+}
